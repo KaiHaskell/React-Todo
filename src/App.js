4ee893ebde78fa78f5cebf3cec150a38cc2330f3
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-01-13 19:08:21 -0500

    Met MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React from "react";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
+
+import "./components/TodoComponents/Todo.css";
+
+const toDoArray = [
+  {
+    todo: "Stretch problems",
+    id: 1,
+    completed: false
+  },
+  {
+    todo: "Make code DRY",
+    id: 2,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      toDoArray: toDoArray
+    };
+  }
+
+  addItem = newItemText => {
+    const newItem = {
+      todo: newItemText,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({ toDoArray: [...this.state.toDoArray, newItem] });
+  };
+
+  clearCompleted = () => {
+    this.setState({
+      toDoArray: this.state.toDoArray.filter(item => {
+        if (item.completed === false) {
+          return {};
+        }
+      })
+    });
+  };
+
+  toggleCompleted = id => {
+    this.setState({
+      toDoArray: this.state.toDoArray.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="main-container">
+        <div className="list-container">
+          <TodoForm
+            addItem={this.addItem}
+            clearCompleted={this.clearCompleted}
+          />
+        </div>
+        <TodoList
+          toggleCompleted={this.toggleCompleted}
+          toDoArray={this.state.toDoArray}
+        />
       </div>
     );
   }
